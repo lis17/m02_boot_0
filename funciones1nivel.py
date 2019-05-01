32a67ce953b0ade34dd93029770836205a476b29
liv jimenez delgado <lisbethdc17@hotmail.com>
2019-05-01 16:39:59 +0200

    Lambdas

---
+++ funciones1nivel.py
@@ -0,0 +1,17 @@
+def normal(i):
+    return i
+
+def cuadrado(x):
+    return x * x
+
+def sumaTodos(limitTo,f):
+    resultado = 0
+    for i in range (limitTo +1):
+        resultado += f(i)
+        
+    return resultado
+
+if __name__ == '__main__':
+    print(sumaTodos(100,normal))
+    print(sumaTodos(3, cuadrado))
+
